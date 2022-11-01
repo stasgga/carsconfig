a6ee2704b3e018e804473def3251102663fdcac6
stasgga <stsggrn25@gmail.com>
2022-11-01 07:15:58 -0400

    finish basic app

---
+++ components/Item.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  label: string;
+  value: any;
+}>();
+</script>
+
+<template>
+  <div>
+    <span class="label font-bold text-gray-600 text-sm uppercase"
+      >{{ label }}:
+    </span>
+    <span class="capitalize">
+      {{ value }}
+    </span>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  css: ["@unocss/reset/tailwind.css"],
+  modules: ["@unocss/nuxt"],
+});
